1ed8b7b6f092a9e9df80f695a5e23827d8919525
dusan <dusanmarkovic1021@gmail.com>
2025-02-22 18:25:46 +0100

    Second exercise solved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (word, times) {
+    if (times <= 0) {
+        return "ERROR!";
+    }
+    const n = 3;
+    let finalWord = "";
+    for (let i = 0; i < 3; i++) {
+        finalWord += word;
+    }
+    return finalWord;
 };
 
 // Do not edit below this line
