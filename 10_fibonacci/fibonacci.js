95c2388fdfd63dc154e2bfee0b37232a3ffddc67
dusan <dusanmarkovic1021@gmail.com>
2025-02-24 15:27:22 +0100

    10th exercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+    let number = n;
 
+    if (typeof n == 'string') {
+        number = Number(n);
+    }
+
+    if (number == 0) return 0;
+    if (number < 0) return "OOPS";
+
+    let num1 = 1;
+    let num2 = 1;
+    for (let i = 2; i < number; i++) {
+        let tmp = num1;
+        num1 = num2;
+        num2 += tmp;
+    }
+    return num2;
 };
 
 // Do not edit below this line
