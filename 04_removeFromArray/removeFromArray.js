a01d55dcf170551b5f5b1418351cbcd2a7951909
dusan <dusanmarkovic1021@gmail.com>
2025-02-24 14:29:08 +0100

    Fourth exercise done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...items) {
+    if (arr.length <= 0) return;
+
+    let reducedArray = [];
+
+    arr.forEach(element => {
+        if (!items.includes(element)) {
+            reducedArray.push(element);
+        }
+    });
+
+    return reducedArray;
 };
 
 // Do not edit below this line
