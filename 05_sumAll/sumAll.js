5025cfe3c820fcc48883442db71c4de59f871738
dusan <dusanmarkovic1021@gmail.com>
2025-02-24 14:37:18 +0100

    Fifth exercise done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (x, y) {
+    if (x < 0 || y < 0) return Error;
+    if (typeof (x) != typeof (1) ||
+        typeof (y) != typeof (1))
+        return Error;
 
+    let sum = 0;
+    if (x > y) return sum;
+    for (let i = x; i <= y; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
